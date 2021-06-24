bd790ec331b6b6ce09f4e6d913e30badb3380eb5
Josep Cristobal <josepc.mercader@gmail.com>
2021-06-24 17:54:10 +0200

    Todos los test OK

--- src/components/auth/LoginPage/LoginForm.test.js
+++ src/components/auth/LoginPage/LoginForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//Método del test elegido shallow
 import { shallow } from 'enzyme';
 
 import LoginForm from './LoginForm';
@@ -29,13 +30,14 @@ describe('LoginForm', () => {
   });
 
   test('should submit credentials', () => {
-    const credentials = { username: 'pepe', password: 'password' };
+    const credentials = { email: 'pepe', password: 'pepepass', check: false };
     const wrapper = render();
 
     const usernameField = wrapper.find('.loginForm-field').at(0);
     usernameField
       .props()
-      .onChange({ target: { name: 'username', value: credentials.username } });
+      .onChange({ target: { name: 'email', value: credentials.email } });
+    
     const passwordField = wrapper.find('.loginForm-field').at(1);
     passwordField
       .props()
@@ -44,8 +46,7 @@ describe('LoginForm', () => {
     const form = wrapper.find('.loginForm');
     form.simulate('submit', { preventDefault: () => {} });
 
-    //TODO no acepta el toBe en mi test
-    //expect(wrapper.find('.loginForm-submit').props().disabled).toBe(true);
-    //expect(props.onSubmit).toHaveBeenCalledWith(credentials);
+    expect(wrapper.find('.loginForm-submit').props().disabled).toBe(false);
+    expect(props.onSubmit).toHaveBeenCalledWith(credentials);
   });
 });
